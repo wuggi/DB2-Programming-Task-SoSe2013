adbf60fd4c20ffa345cd2406fb034711069b82c5
wuggi <patrick.wuggazer@st.ovgu.de>
2013-06-15 16:42:08 +0200

    run_length_encoding implementation ++not finished++

--- compression/run_length_encoding.hpp
+++ compression/run_length_encoding.hpp
@@ -1,6 +1,6 @@
 
 /*! \example dictionary_compressed_column.hpp
- * This is an example of how to implement a compression technique in our framework. One has to inherit from an abstract base class CoGaDB::CompressedColumn and implement the pure virtual methods.
+ * This is an example of how to implement a compression technique in our framework. One has to inherit from an abstract base class CoGaDB::RunLengthEncoding and implement the pure virtual methods.
  */
 
 #pragma once
@@ -14,48 +14,52 @@ namespace CoGaDB{
  *  \brief     This class represents a dictionary compressed column with type T, is the base class for all compressed typed column classes.
  */
 template<class T>
-class RunLengthEncoding : public CompressedColumn<T>{
+class RunLengthEncoding : public RunLengthEncoding<T>{
     public:
     /***************** constructors and destructor *****************/
     RunLengthEncoding(const std::string& name, AttributeType db_type);
     virtual ~RunLengthEncoding();
 
-    virtual bool insert(const boost::any& new_Value);
-    virtual bool insert(const T& new_value);
-    template <typename InputIterator>
-    bool insert(InputIterator first, InputIterator last);
+    virtual bool insert(const boost::any& new_value)  = 0;
+    virtual bool insert(const T& new_value)  = 0;
 
-    virtual bool update(TID tid, const boost::any& new_value);
-    virtual bool update(PositionListPtr tid, const boost::any& new_value);
+    virtual bool update(TID tid, const boost::any& new_value) = 0;
+    virtual bool update(PositionListPtr tid, const boost::any& new_value) = 0;
 
-    virtual bool remove(TID tid);
+    virtual bool remove(TID tid)=0;
     //assumes tid list is sorted ascending
-    virtual bool remove(PositionListPtr tid);
-    virtual bool clearContent();
+    virtual bool remove(PositionListPtr tid)=0;
+    virtual bool clearContent()=0;
 
-    virtual const boost::any get(TID tid);
+    virtual const boost::any get(TID tid) = 0;
     //virtual const boost::any* const getRawData()=0;
-    virtual void print() const throw();
-    virtual size_t size() const throw();
-    virtual unsigned int getSizeinBytes() const throw();
+    virtual void print() const throw() = 0;
+    virtual size_t size() const throw() = 0;
+    virtual unsigned int getSizeinBytes() const throw() = 0;
 
-    virtual const ColumnPtr copy() const;
+    virtual const ColumnPtr copy() const = 0;
 
-    virtual bool store(const std::string& path);
-    virtual bool load(const std::string& path);
+    virtual bool store(const std::string& path)  = 0;
+    virtual bool load(const std::string& path)  = 0;
+    virtual bool isMaterialized() const  throw();
+
+    virtual bool isCompressed() const  throw();
 
 
+    virtual T& operator[](const int index)  = 0;
 
-    virtual T& operator[](const int index);
+    std::vector<T> values_;
+    std::vector<int> cnt;
 
 };
+
 
 
 /***************** Start of Implementation Section ******************/
 
 
     template<class T>
-    RunLengthEncoding<T>::RunLengthEncoding(const std::string& name, AttributeType db_type) : CompressedColumn<T>(name, db_type){
+    RunLengthEncoding<T>::RunLengthEncoding(const std::string& name, AttributeType db_type) : ColumnBaseTyped<T>(name,db_type){
 
     }
 
@@ -65,87 +69,173 @@ class RunLengthEncoding : public CompressedColumn<T>{
     }
 
     template<class T>
-    bool RunLengthEncoding<T>::insert(const boost::any&){
-
+    bool RunLengthEncoding<T>::isMaterialized() const  throw(){
         return false;
     }
 
     template<class T>
-    bool RunLengthEncoding<T>::insert(const T&){
-        return false;
-    }
-
-    template <typename T>
-    template <typename InputIterator>
-    bool RunLengthEncoding<T>::insert(InputIterator , InputIterator ){
-
+    bool RunLengthEncoding<T>::isCompressed() const  throw(){
         return true;
     }
 
-    template<class T>
-    const boost::any RunLengthEncoding<T>::get(TID){
 
-        return boost::any();
+
+    template<class T>
+    bool RunLengthEncoding<T>::insert(const boost::any& new_value){
+
+        if(new_value.empty()) return false;
+
+        if(typeid(T)==new_value.type()){
+             T value = boost::any_cast<T>(new_value);
+
+             for(int i=0;i<values_.size();i++)
+             {
+                 if(values_[i]==new_value)
+                 {
+                     cnt[i]=cnt[i]+1;
+                     return true;
+                 }
+             }
+
+             values_.push_back(value);
+             cnt.push_back(1);
+             return true;
+        }
+        return false;
     }
 
+    bool RunLengthEncoding<T>:: insert(const T& new_value)
+    {
+        for(int i=0;i<values_.size();i++)
+        {
+            if(values_.at(i)==new_value)
+            {
+                cnt.at(i)=cnt.at(i)+1;
+                return true;
+            }
+        }
+
+        values_.push_back(value);
+        cnt.push_back(1);
+        return true;
+    }
+
+
+    template<class T>
+    const boost::any RunLengthEncoding<T>::get(TID tid){
+        if(tid<values_.size())
+            return boost::any(values_[tid]);
+        else{
+            std::cout << "fatal Error!!! Invalid TID!!! Attribute: " << this->name_ << " TID: " << tid  << std::endl;
+        }
+        return boost::any();
+
+    }
+//TODO: cnt mit ausgeben
     template<class T>
     void RunLengthEncoding<T>::print() const throw(){
+
+        std::cout << "| " << this->name_ << " |" << std::endl;
+        std::cout << "________________________" << std::endl;
+        for(unsigned int i=0;i<values_.size();i++){
+            std::cout << "| " << values_[i] << " |" << std::endl;
+        }
 
     }
     template<class T>
     size_t RunLengthEncoding<T>::size() const throw(){
 
-        return 0;
+        return values_.size();
     }
     template<class T>
     const ColumnPtr RunLengthEncoding<T>::copy() const{
 
-        return ColumnPtr();
+        return ColumnPtr(new Column<T>(*this));
     }
 
     template<class T>
-    bool RunLengthEncoding<T>::update(TID , const boost::any& ){
+    bool RunLengthEncoding<T>::store(const std::string& path){
+
+        //string path("data/");
+        std::string path_(path);
+        path_ += "/";
+        path_ += this->_;
+        //std::cout << "Writing Column " << this->getName() << " to File " << path << std::endl;
+        std::ofstream outfile (path_.c_str(),std::ios_base::binary | std::ios_base::out);
+        boost::archive::binary_oarchive oa(outfile);
+
+        oa << values_;
+
+        outfile.flush();
+        outfile.close();
+        return true;
+    }
+    template<class T>
+    bool RunLengthEncoding<T>::load(const std::string& path){
+
+        std::string path_(path);
+        //std::cout << "Loading column '" << this->name_ << "' from path '" << path << "'..." << std::endl;
+        //string path("data/");
+        path_ += "/";
+        path_ += this->name_;
+
+        //std::cout << "Opening File '" << path << "'..." << std::endl;
+        std::ifstream infile (path_.c_str(),std::ios_base::binary | std::ios_base::in);
+        boost::archive::binary_iarchive ia(infile);
+        ia >> values_;
+        infile.close();
+
+
+        return true;
+    }
+    template<class T>
+    bool RunLengthEncoding<T>::isMaterialized() const  throw(){
+
         return false;
     }
 
     template<class T>
-    bool RunLengthEncoding<T>::update(PositionListPtr , const boost::any& ){
-        return false;
+    bool RunLengthEncoding<T>::isCompressed() const  throw(){
+        return true;
     }
 
     template<class T>
-    bool RunLengthEncoding<T>::remove(TID){
-        return false;
-    }
+    T& RunLengthEncoding<T>::operator[](const int index){
 
-    template<class T>
-    bool RunLengthEncoding<T>::remove(PositionListPtr){
-        return false;
-    }
-
-    template<class T>
-    bool RunLengthEncoding<T>::clearContent(){
-        return false;
-    }
-
-    template<class T>
-    bool RunLengthEncoding<T>::store(const std::string&){
-        return false;
-    }
-    template<class T>
-    bool RunLengthEncoding<T>::load(const std::string&){
-        return false;
-    }
-
-    template<class T>
-    T& RunLengthEncoding<T>::operator[](const int){
-        static T t;
-        return t;
+        return values_[index];
     }
 
     template<class T>
     unsigned int RunLengthEncoding<T>::getSizeinBytes() const throw(){
-        return 0; //return values_.capacity()*sizeof(T);
+        return values_.capacity()*sizeof(T);
+    }
+
+    template<class T>
+    bool RunLengthEncoding<T>::update(TID tid, const boost::any& new_value){
+
+        cnt[tid]=cnt[tid]-1;
+        return this->insert(new_value);
+
+    }
+
+    template<class T>
+    bool RunLengthEncoding<T>::update(PositionListPtr tid , const boost::any& new_value){
+        return false;
+    }
+
+    template<class T>
+    bool DictionaryCompressedColumn<T>::remove(TID){
+        return false;
+    }
+
+    template<class T>
+    bool DictionaryCompressedColumn<T>::remove(PositionListPtr){
+        return false;
+    }
+
+    template<class T>
+    bool DictionaryCompressedColumn<T>::clearContent(){
+        return false;
     }
 
 /***************** End of Implementation Section ******************/
@@ -153,4 +243,5 @@ class RunLengthEncoding : public CompressedColumn<T>{
 
 
 }; //end namespace CogaDB
+
 
